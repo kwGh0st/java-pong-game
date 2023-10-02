24f2c1a5a775edc448caa058970ee149de3e5047
Karol Programuje <karol341654@gmail.com>
2023-10-02 14:50:14 +0200

    Creating GameFrame

--- src/main/java/pong/GameFrame.java
+++ src/main/java/pong/GameFrame.java
@@ -1,4 +1,20 @@
 package main.java.pong;
 
-public class GameFrame {
+import javax.swing.*;
+import java.awt.*;
+
+public class GameFrame extends JFrame {
+    private GamePanel panel;
+
+    public GameFrame() {
+        panel = new GamePanel();
+        this.add(panel);
+        this.setTitle("Pong Game");
+        this.setResizable(false);
+        this.setBackground(Color.black);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
 }
