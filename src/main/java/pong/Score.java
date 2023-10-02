e71ca6ceb2277f87a6e89a7c43857b13d8aaaa00
Karol Programuje <karol341654@gmail.com>
2023-10-02 14:51:01 +0200

    adding fields, method in Score class.

--- src/main/java/pong/Score.java
+++ src/main/java/pong/Score.java
@@ -1,4 +1,18 @@
 package main.java.pong;
 
-public class Score {
+import java.awt.*;
+
+public class Score extends Rectangle {
+    private static int GAME_WIDTH;
+    private static int GAME_HEIGHT;
+    private int player1;
+    private int player2;
+
+    public Score(int gameWidth, int gameHeight) {
+
+    }
+
+    public void draw(Graphics g) {
+
+    }
 }
