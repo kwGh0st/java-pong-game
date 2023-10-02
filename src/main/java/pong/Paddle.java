d553332036abc2f1da5b3da2a76384c8f517b022
Karol Programuje <karol341654@gmail.com>
2023-10-02 14:50:42 +0200

    adding fields, method in Paddle class.

--- src/main/java/pong/Paddle.java
+++ src/main/java/pong/Paddle.java
@@ -1,4 +1,27 @@
 package main.java.pong;
 
-public class Paddle {
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+public class Paddle extends Rectangle{
+    private int id;
+    private int yVelocity;
+
+    public Paddle() {
+    }
+
+    public void keyPressed(KeyEvent e) {}
+    public void keyReleased(KeyEvent e) {
+
+    }
+    public void setYDirection(int yDirection) {
+
+    }
+    public void move() {
+
+    }
+    public void draw(Graphics g) {
+
+    }
+
 }
