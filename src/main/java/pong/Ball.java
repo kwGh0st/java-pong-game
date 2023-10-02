600dd046acce71425b22be73d35f886f152b397e
Karol Programuje <karol341654@gmail.com>
2023-10-02 14:49:49 +0200

    Adding fields, methods in Ball class

--- src/main/java/pong/Ball.java
+++ src/main/java/pong/Ball.java
@@ -1,4 +1,29 @@
 package main.java.pong;
 
-public class Ball {
+import java.awt.*;
+import java.util.Random;
+
+public class Ball extends Rectangle {
+    private Random random;
+    private int xVelocity;
+    private int yVelocity;
+
+    public Ball() {
+    }
+
+    public void setXDirection(int randomXDirection) {
+
+    }
+    public void setYDirection(int randomYDirection) {
+
+    }
+
+    public void move() {
+
+    }
+
+    public void draw(Graphics g) {
+
+    }
+
 }
